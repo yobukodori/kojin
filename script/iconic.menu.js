eeb1816d4cb4251691a91a89accac47da977feca
yobukodori <32874862+yobukodori@users.noreply.github.com>
2019-09-20 12:34:05 +0900

    Add files via upload

--- script/iconic.menu.js
+++ script/iconic.menu.js
@@ -9,7 +9,7 @@
 	let d = document;
 	iconicMenu.items.forEach(item=>{
 			let e = d.createElement("div"), targetBlank = iconicMenu.options && iconicMenu.options.targetBlank;
-			e.innerHTML = '<a href="' + item.url + '" '+(targetBlank ? 'target="_blank"':'')+'><div><img class="item-icon" src="' + item.icon + '"><div class="item-name">' + item.name + '</div></div></a>';
+			e.innerHTML = '<a href="' + item.url + '" '+(targetBlank ? 'target="_blank"':'')+'><div><img class="item-icon" src="' + (item.icon ? item.icon : 'icon/na.png')+ '"><div class="item-name">' + item.name + '</div></div></a>';
 			e.className = "item";
 			d.getElementById("iconic-menu-container").appendChild(e);
 	});
