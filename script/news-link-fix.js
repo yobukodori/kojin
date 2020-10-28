eee5faa24a1c37ec438fe83f2b5275dda004acdf
yobukodori <32874862+yobukodori@users.noreply.github.com>
2020-10-28 23:17:53 +0900

    Add files via upload

--- script/news-link-fix.js
+++ script/news-link-fix.js
@@ -1,5 +1,5 @@
 /*
- * title: news link fix v.0.1.17
+ * title: news link fix v.0.1.18
  * name: news-link-fix.js
  * author: yobukodori
 */
@@ -167,15 +167,24 @@
 						return response.text();
 					})
 					.then(function(html) {
+						let b, p, title, src, href;
 						{
-							let title = "n/a";
-							let b, s, r, p, c;
-							(b = str_find_block(html,'class="pickupMain_articleTitle_text">','<')) && !b.error && (title = html.substring(b.first,b.last)) && (p = e.querySelector('.topics_item_title')) && (p.innerText = decodeEntities(title).replace(/\u3000/g," "));
+							(b = str_find_block_r(html,'>','</h2>')) && !b.error && (title = html.substring(b.first,b.last));
+							title && (b = str_find_block_r(html,'>','</span>', b.next)) && !b.error && (src = html.substring(b.first,b.last))
 						}
-						let sig = 'class="pickupMain_articleTitle', i = html.indexOf(sig), r;
-						i != -1 && (r = html.substring(i + sig.length, html.indexOf('data-ual-gotocontent', i + sig.length)).match('href="(.+?)"')) && (e.href = r[1]);
-						let parent = e.querySelector('div.topics_item_sub'), b, src, span;
-						(b = str_find_block(html,'class="pickupMain_media">','<')) && !b.error && (src = html.substring(b.first,b.last)) && parent &&  (span = d.createElement("span")) && (span.className = "newsFeed_item_media") && (span.setAttribute("style","vertical-align: bottom"),!0) && (span.innerText = src) && parent.appendChild(span);
+						if (title){
+							(p = e.querySelector('.topics_item_title')) && (p.innerText = decodeEntities(title).replace(/\u3000/g," "));
+						}
+						if (href){
+							e.href = href;
+						}
+						if (src){
+							let parent = e.querySelector('div.topics_item_sub'), span;
+							parent &&  (span = d.createElement("span")) && (span.className = "newsFeed_item_media") && (span.setAttribute("style","vertical-align: bottom"),!0) && (span.innerText = src) && parent.appendChild(span);
+						}
+					})
+					.catch(err=>{
+						console.log("catch:",err);
 					});			
 				}
 			},
