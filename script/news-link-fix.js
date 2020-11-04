403ed4764d73d0cddf59db758cad33a81457f356
yobukodori <32874862+yobukodori@users.noreply.github.com>
2020-11-04 15:48:01 +0900

    Add files via upload

--- script/news-link-fix.js
+++ script/news-link-fix.js
@@ -87,7 +87,7 @@
 	
 	function censor(art){
 		if (art.src){
-			art.ng = /スポーツ報知|スポーツアネックス|スポニチアネックス|東スポWeb|Smart FLASH|デイリースポーツ|日刊スポーツ|サーチナ|リアルライブ|Techinsight|スポーツソウル日本版|女性自身|ナリナリドットコム|オリコン/.test(art.src);
+			art.ng = /スポーツ報知|スポーツアネックス|スポニチアネックス|東スポWeb|Smart FLASH|デイリースポーツ|日刊スポーツ|サーチナ|リアルライブ|Techinsight|スポーツソウル日本版|女性自身|ナリナリドットコム|オリコン|週刊女性|おくたま経済新聞/.test(art.src);
 		}
 		if (art.ng){
 			art.original = art.title;
