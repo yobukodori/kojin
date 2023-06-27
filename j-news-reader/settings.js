444a1dff4e948499b85de92df9eb7bc7ed723d49
yobukodori <32874862+yobukodori@users.noreply.github.com>
2023-06-28 01:10:14 +0900

    Add files via upload

--- j-news-reader/settings.js
+++ j-news-reader/settings.js
@@ -3,20 +3,30 @@ const settings = {
 	data: {
 		ngChannel: {},
 		titleFilter: "",
-		ngYahooCategory: {sports:true},
+		ngYahooCategory: {},
 		yahooGetSource: false,
 		yahooMediaFilter: "",
+		afpbbNgCategory: {},
+		compareDatesOnSameUrl: false,
+		excludePayedArticle: false,
+		yomiuriTagFilter: "",
 	},
 	getActiveChannelCount(){
 		return Object.keys(this.profiles).length - Object.keys(this.data.ngChannel).length;
 	},
+	needsToExcludePayedArticle(){
+		return this.data.excludePayedArticle;
+	},
 	needsToGetYahooSource(){
 		return this.data.yahooGetSource;
+	},
+	needsToMarkNewOnSameUrl(){
+		return this.data.compareDatesOnSameUrl;
 	},
 	load(){
 		let v = localStorage.getItem("settings");
 		if (v){
-			this.data = JSON.parse(v);
+			this.data = Object.assign(this.data, JSON.parse(v));
 		}
 	},
 	save(){
@@ -31,13 +41,20 @@ const settings = {
 	isNgTitle(title){
 		return this.data.titleFilter ? new Filter(this.data.titleFilter).match(title) : false;
 	},
-	isNgYahooCategory(id){
+	isAfpbbNgCategory(name){
+		return this.data.afpbbNgCategory[name];
+	},
+	isYomiuriNgTag(tags){
+		if (this.data.yomiuriTagFilter){
+			tags = tags || [];
+			const f = new Filter(this.data.yomiuriTagFilter);
+			return tags.some(tag => f.match(tag));
+		}
+	},
+	isYahooNgCategory(id){
 		return this.data.ngYahooCategory[id];
 	},
-	isActiveYahooCategory(id){
-		return ! this.isNgYahooCategory(id);
-	},
-	isNgYahooMeida(title){
+	isYahooNgMeida(title){
 		return this.data.yahooMediaFilter ? new Filter(this.data.yahooMediaFilter).match(title) : false;
 	},
 	init(profiles){
@@ -47,7 +64,7 @@ const settings = {
 	},
 	open(){
 		const id = "settings-modal";
-		if (document.getElementById(id)){ return; }
+		if (document.getElementById(id)){ throw Error("settings.open already opened"); }
 		const modal = document.createElement("div");
 		modal.id = id;
 		modal.insertAdjacentHTML("beforeend", 
@@ -57,13 +74,32 @@ const settings = {
 	<button class="settings-close">閉じる</button>
 	</div>
 	<div id="settings-channels">
-		<b>チャンネル選択</b>
+		<b>チャンネル選択</b><button class="set-all">すべて選択</button><button class="clear-all">すべて解除</button>
 		<div class="container checkbox">
 		</div>
 	</div>
 	<div id="settings-title-filter">
 		<b>タイトルが次のフィルタに一致する記事を除外する</b>
 		<div><textarea rows="5" spellcheck="false"></textarea></div>
+	</div>
+	<div>
+		<div><input type="checkbox" id="exclude-payed-article">
+			<label for="exclude-payed-article"><b>有料記事を除外する</b></label></div>
+	</div>
+	<div>
+		<div><input type="checkbox" id="afpbb-exclude-sports">
+			<label for="afpbb-exclude-sports"><b>AFPBBのスポーツ記事を除外する</b></label></div>
+	</div>
+	<div>
+		<div><input type="checkbox" id="compare-dates-on-same-url">
+			<label for="compare-dates-on-same-url"><b>記事URLが同じでも日付が新しければ新着とする</b></label></div>
+	</div>
+	<div class="channel yomiuri">
+		<b>読売新聞設定</b>
+		<div class="filter tag">
+			<b>タグが次のフィルタに一致する記事を除外する</b>
+			<div><textarea rows="1" spellcheck="false"></textarea></div>
+		</div>
 	</div>
 	<div id="settings-yahoo">
 		<b>Yahoo!ニュース設定</b>
@@ -72,7 +108,7 @@ const settings = {
 			<div class="container checkbox">
 			</div>
 		</div>
-		<div id="settings-yahoo-get-source">
+		<div>
 			<div><input type="checkbox" id="sy-get-source">
 				<label for="sy-get-source"><b>元記事のタイトルとメディア名を取得する</b></label></div>
 		</div>
@@ -97,6 +133,16 @@ const settings = {
 				ev.target.checked ? delete this.data.ngChannel[id] : this.data.ngChannel[id] = true;
 			});
 		});
+		dlg.querySelector('#settings-channels button.set-all').addEventListener("click", ev =>{
+			dlg.querySelectorAll('#settings-channels > .container input').forEach(e =>{
+				! e.checked && e.click();
+			});
+		});
+		dlg.querySelector('#settings-channels button.clear-all').addEventListener("click", ev =>{
+			dlg.querySelectorAll('#settings-channels > .container input').forEach(e =>{
+				e.checked && e.click();
+			});
+		});
 		// タイトルフィルタ
 		let tfilter = dlg.querySelector("#settings-title-filter textarea");
 		tfilter.value = this.data.titleFilter || "";
@@ -112,10 +158,43 @@ const settings = {
 		tfilter.addEventListener("blur", ev =>{
 			updateTitleFilter();
 		});
+		// 有料記事を除外
+		e = dlg.querySelector("#exclude-payed-article");
+		this.data.excludePayedArticle && (e.checked = true);
+		e.addEventListener("change", ev =>{
+			this.data.excludePayedArticle = ev.target.checked ? true : false;
+		});
+		// AFPBBスポーツ記事除外
+		e = dlg.querySelector("#afpbb-exclude-sports");
+		this.data.afpbbNgCategory.sports && (e.checked = true);
+		e.addEventListener("change", ev =>{
+			this.data.afpbbNgCategory.sports = ev.target.checked ? true : false;
+		});
+		// 同一URLの場合日付を比較
+		e = dlg.querySelector("#compare-dates-on-same-url");
+		this.data.compareDatesOnSameUrl && (e.checked = true);
+		e.addEventListener("change", ev =>{
+			this.data.compareDatesOnSameUrl = ev.target.checked ? true : false;
+		});
+		// 読売新聞タグフィルター
+		let ymfilter = dlg.querySelector(".channel.yomiuri .filter.tag textarea");
+		ymfilter.value = this.data.yomiuriTagFilter || "";
+		const updateYomiuriTagFilter = function(){
+			const v = ymfilter.value.trim(), f = v && new Filter(v);
+			if (f && f.error){
+				alert("読売タグフィルタが不正です。" + f.error);
+				return false;
+			}
+			settings.data.yomiuriTagFilter = v;
+			return true;
+		};
+		ymfilter.addEventListener("blur", ev =>{
+			updateYomiuriTagFilter();
+		});
 		// Yahoo カテゴリ選択
 		c = dlg.querySelector('#settings-yahoo-categories > .container');
 		yahoo.categories.forEach(ca =>{
-			let id = ca.id, checked = this.isActiveYahooCategory(id) ? "checked" : "";
+			let id = ca.id, checked = this.isYahooNgCategory(id) ? "" : "checked";
 			c.insertAdjacentHTML("beforeend", 
 			`<div><input type="checkbox" id="yca-${id}" ${checked}><label for="yca-${id}">${ca.name}</label></div>`);
 			c.querySelector("#yca-" + id).addEventListener("change", ev =>{
@@ -131,7 +210,7 @@ const settings = {
 		// Yahoo メディアフィルタ
 		let yfilter = dlg.querySelector("#settings-yahoo-media-filter textarea");
 		yfilter.value = this.data.yahooMediaFilter || "";
-		const updateYahoFilter = function(){
+		const updateYahooMediaFilter = function(){
 			const v = yfilter.value.trim(), f = v && new Filter(v);
 			if (f && f.error){
 				alert("Yahooメディアフィルタが不正です。" + f.error);
@@ -141,10 +220,10 @@ const settings = {
 			return true;
 		};
 		yfilter.addEventListener("blur", ev =>{
-			updateYahoFilter();
+			updateYahooMediaFilter();
 		});
 		
-		let onkeydown, cleanup, close;
+		let onkeydown, cleanup, close, resolve;
 		onkeydown = function(ev){
 			if (document.elementFromPoint(0,0) === modal){
 				ev.key === "Escape" && cleanup();
@@ -154,10 +233,14 @@ const settings = {
 		cleanup = function(){
 			document.removeEventListener("keydown", onkeydown);
 			modal.remove();
+			setTimeout(resolve, 0, true);
 		};
 		close = function(){
-			let t = updateTitleFilter(), y = updateYahoFilter();
-			if (! (t && y)){ return; }
+			let error = 0;
+			[updateTitleFilter, updateYomiuriTagFilter, updateYahooMediaFilter].forEach(update =>{
+				! update() && error++;
+			});
+			if (error){ return; }
 			cleanup();
 		};
 		modal.querySelector('button.settings-close').addEventListener("click", ev => close());
@@ -165,5 +248,9 @@ const settings = {
 		dlg.addEventListener("click", ev => ev.stopPropagation());
 
 		document.body.appendChild(modal);
+		
+		return new Promise((_resolve, reject)=>{
+			resolve = _resolve;
+		});
 	},
 };
