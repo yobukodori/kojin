b3ad6724ca4b90fe584e4afe4b06fdeb7f0b6093
yobukodori <32874862+yobukodori@users.noreply.github.com>
2024-02-19 08:33:00 +0900

    Add files via upload

--- script-for-me/dark-mode-news.js
+++ script-for-me/dark-mode-news.js
@@ -1,5 +1,5 @@
 //name dark mode news
-//; v.1.0.2 beta
+//; v.1.0.2 (2024/2/19 8:26)
 //matches https://www.jiji.com/*, https://www.47news.jp/*, https://www.yomiuri.co.jp/*, https://www.asahi.com/*, https://mainichi.jp/*, https://*.nhk.or.jp/*, https://www.nikkei.com/*, https://jp.reuters.com/*, https://www.cnn.co.jp/*, https://www.bbc.com/*, https://www.afpbb.com/*, https://forbesjapan.com/*, https://news.yahoo.co.jp/*, https://www.bloomberg.co.jp/*
 //option start
 //js
@@ -42,6 +42,10 @@
 			const className = "dark-mode-news-dark-mode",
 				id = className + "-style",
 				buttonId = "dark-mode-news-recovery-button";
+			let ng = "img, svg";
+			if (location.hostname === "www.afpbb.com"){
+				ng += ", .next-btn, .prev-btn, .thumbtitle";
+			}
 			let e = document.createElement("style");
 			e.id = id;
 			e.textContent = `
@@ -50,7 +54,7 @@
 					color: CanvasText;
 					color-scheme: ${requiredColorScheme};
 				}
-				.${className} *:not(img, svg) {
+				.${className} *:not(${ng}) {
 					background-color: Canvas !important;
 					color: CanvasText !important;
 				}
